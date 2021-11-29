161fa4149063c9db601e0075255a3edd0106b30a
Slymaster <sylvain.rey.pro@gmail.com>
2021-11-29 16:54:08 +0100

    env file

--- truffle-config.js
+++ truffle-config.js
@@ -10,6 +10,11 @@ module.exports = {
       port: 8545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
     },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(`${process.env.MNEMONIC}`, `https://ropsten.infura.io/v3/${process.env.INFURA_ID}`)
+      }
+    }
   },
   mocha: {
     timeout: 100000
