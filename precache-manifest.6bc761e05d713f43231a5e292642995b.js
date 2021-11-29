2b0d1e42a7bb2fc2e859a0176a592366d01e7bdd
Slymaster <sylvain.rey.pro@gmail.com>
2021-11-29 17:05:01 +0100

    Updates

---
+++ precache-manifest.6bc761e05d713f43231a5e292642995b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab0f1ddcc24ec551bdc26c839b5f095f",
+    "url": "/voting_dapp/index.html"
+  },
+  {
+    "revision": "4fe26998014965dea7b0",
+    "url": "/voting_dapp/static/css/2.fa43bc9e.chunk.css"
+  },
+  {
+    "revision": "d286f9d836d22b0ee42c",
+    "url": "/voting_dapp/static/css/main.94554a85.chunk.css"
+  },
+  {
+    "revision": "4fe26998014965dea7b0",
+    "url": "/voting_dapp/static/js/2.2216b7ae.chunk.js"
+  },
+  {
+    "revision": "d286f9d836d22b0ee42c",
+    "url": "/voting_dapp/static/js/main.5785a166.chunk.js"
+  },
+  {
+    "revision": "4330d459ea17a06d0a5b",
+    "url": "/voting_dapp/static/js/runtime-main.448d91aa.js"
+  }
+]);
